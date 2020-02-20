7f070a62348a8a4848316bf16870be54b4dd0c5b
Niko Matsakis <niko@alum.mit.edu>
2020-02-20 10:33:57 -0500

    add various updates, like status and nicer colors

--- src/graphviz.rs
+++ src/graphviz.rs
@@ -1,4 +1,4 @@
-use crate::tree::{Group, SkillTree};
+use crate::tree::{Group, SkillTree, Status};
 use fehler::throws;
 use std::io::Write;
 
@@ -23,8 +23,8 @@ pub(crate) fn write_graphviz(tree: &SkillTree, output: &mut dyn Write) {
                 writeln!(
                     output,
                     r#"{} -> {};"#,
-                    port_name(requirement),
-                    port_name(&group.name),
+                    port_name(requirement, "out"),
+                    port_name(&group.name, "in"),
                 )?;
             }
         }
@@ -35,7 +35,7 @@ pub(crate) fn write_graphviz(tree: &SkillTree, output: &mut dyn Write) {
                     writeln!(
                         output,
                         r#"{} -> "{}":{};"#,
-                        port_name(requirement),
+                        port_name(requirement, "out"),
                         group.name,
                         item.port.as_ref().expect("missing port"),
                     )?;
@@ -47,6 +47,13 @@ pub(crate) fn write_graphviz(tree: &SkillTree, output: &mut dyn Write) {
     writeln!(output, r#"}}"#)?;
 }
 
+const STOP_SIGN_EMOJI: &str = "🛑";
+const WATCH_EMOJI: &str = "⌚";
+const BALLOT_BOX_EMOJI: &str = "☐";
+const HAMMER_WRENCH_EMOJI: &str = "🛠️";
+const CHECKED_BOX_EMOJI: &str = "☑️";
+const RAISED_HAND_EMOJI: &str = "🙋";
+
 #[throws(anyhow::Error)]
 fn write_group_label(group: &Group, output: &mut dyn Write) {
     writeln!(output, r#"  label = <<table>"#)?;
@@ -55,36 +62,70 @@ fn write_group_label(group: &Group, output: &mut dyn Write) {
 
     writeln!(
         output,
-        r#"    <tr><td bgcolor="gray" port="all">{}</td></tr>"#,
+        r#"    <tr><td bgcolor="darkgoldenrod" port="all" colspan="2">{}</td></tr>"#,
         label,
     )?;
 
     for item in &group.items {
-        let href = attribute_str("href", &item.href);
-        let port = attribute_str("port", &item.port);
+        let item_status = item.status.or(group.status).unwrap_or(Status::Unassigned);
+        let (emoji, fontcolor, start_tag, end_tag) = match item_status {
+            Status::Blocked => (
+                WATCH_EMOJI,
+                None,
+                "<i><font color=\"lightgrey\">",
+                "</font></i>",
+            ),
+            Status::Unassigned => (RAISED_HAND_EMOJI, Some("red"), "", ""),
+            Status::Assigned => (HAMMER_WRENCH_EMOJI, None, "", ""),
+            Status::Complete => (CHECKED_BOX_EMOJI, None, "<s>", "</s>"),
+        };
+
+        let fontcolor = attribute_str("fontcolor", &fontcolor, "");
+        let bgcolor = attribute_str("bgcolor", &Some("cornsilk"), "");
+        let href = attribute_str("href", &item.href, "");
+        let start_under = if item.href.is_some() { "<u>" } else { "" };
+        let end_under = if item.href.is_some() { "</u>" } else { "" };
+        let port_in = attribute_str("port", &item.port, "in");
+        let port_out = attribute_str("port", &item.port, "out");
         writeln!(
             output,
-            r#"    <tr><td{}{}>{}</td></tr>"#,
-            href, port, item.label
+            "    \
+             <tr>\
+             <td{bgcolor}{port_in}>{emoji}</td>\
+             <td{fontcolor}{bgcolor}{href}{port_out}>\
+             {start_tag}{start_under}{label}{end_under}{end_tag}\
+             </td>\
+             </tr>",
+            fontcolor = fontcolor,
+            bgcolor = bgcolor,
+            emoji = emoji,
+            href = href,
+            port_in = port_in,
+            port_out = port_out,
+            label = item.label,
+            start_tag = start_tag,
+            end_tag = end_tag,
+            start_under = start_under,
+            end_under = end_under,
         )?;
     }
 
     writeln!(output, r#"  </table>>"#)?;
 }
 
-fn port_name(requires: &str) -> String {
+fn port_name(requires: &str, mode: &str) -> String {
     if let Some(index) = requires.find(":") {
         let name = &requires[..index];
         let port = &requires[index + 1..];
-        format!(r#""{}":{}"#, name, port)
+        format!(r#""{}":{}_{}"#, name, port, mode)
     } else {
         format!(r#""{}":all"#, requires)
     }
 }
 
-fn attribute_str(label: &str, text: &Option<String>) -> String {
+fn attribute_str(label: &str, text: &Option<impl AsRef<str>>, suffix: &str) -> String {
     match text {
         None => format!(""),
-        Some(t) => format!(" {}=\"{}\"", label, t),
+        Some(t) => format!(" {}=\"{}{}\"", label, t.as_ref(), suffix),
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -14,7 +14,11 @@ pub(crate) struct Group {
     pub(crate) requires: Option<Vec<String>>,
     pub(crate) items: Vec<Item>,
     pub(crate) width: Option<f64>,
+    pub(crate) status: Option<Status>,
 }
+
+#[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub struct GroupIndex(pub usize);
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct Item {
@@ -22,6 +26,25 @@ pub(crate) struct Item {
     pub(crate) href: Option<String>,
     pub(crate) port: Option<String>,
     pub(crate) requires: Option<Vec<String>>,
+    pub(crate) status: Option<Status>,
+}
+
+#[derive(Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub struct ItemIndex(pub usize);
+
+#[derive(Copy, Clone, Debug, Deserialize)]
+pub enum Status {
+    /// Can't work on it now
+    Blocked,
+
+    /// Would like to work on it, but need someone
+    Unassigned,
+
+    /// People are actively working on it
+    Assigned,
+
+    /// This is done!
+    Complete,
 }
 
 impl SkillTree {
@@ -36,6 +59,10 @@ impl SkillTree {
 
     pub(crate) fn groups(&self) -> impl Iterator<Item = &Group> {
         self.group.iter()
+    }
+
+    pub(crate) fn groups_enumerated(&self) -> impl Iterator<Item = (&Group, GroupIndex)> {
+        self.group.iter().zip((0..).map(GroupIndex))
     }
 }
 
@@ -55,6 +82,10 @@ impl Group {
 
     pub(crate) fn items(&self) -> impl Iterator<Item = &Item> {
         self.items.iter()
+    }
+
+    pub(crate) fn items_enumerated(&self) -> impl Iterator<Item = (&Item, ItemIndex)> {
+        self.items.iter().zip((0..).map(ItemIndex))
     }
 }
 
